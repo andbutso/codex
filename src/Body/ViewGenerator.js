963922338ee529a34e150b0396c0f40ef1b4a638
Antonin Bacot <antonin.bacot@gmail.com>
2017-01-31 17:35:27 -0500

    new main view with manual code zoom. next step if flex, then touch zoom

--- src/Body/ViewGenerator.js
+++ src/Body/ViewGenerator.js
@@ -1,29 +1,39 @@
 import React from 'react';
-var zoomLevel = 40;
+var FullChassis = require('./Day.js');
+var zoomLevel = 19;
+var genericStyle = {
+  height: "100%",
+  width: "100%",
+  display:"inline-flex",
+  border: "solid",
+  borderColor: "#111",
+  borderWidth: 1
+};
 
 var OneDay = React.createClass({
   render: function(){
-    var headerStyle = {
-      height: 20,
+    var specificStyle = {
+      textAlign: 'center',
       display:"inline-flex",
-      flexDirection:'row'
+      flexDirection:'column'
     };
     return(
-      <div style={headerStyle}>
+      <div style={Object.assign(genericStyle, specificStyle)}>
+        1
       </div>
     );
   }
 });
 
+
 var ThreeDay = React.createClass({
   render: function(){
-    var headerStyle = {
-      height: 20,
+    var specificStyle = {
       display:"inline-flex",
       flexDirection:'row'
     };
     return(
-      <div style={headerStyle}>
+      <div style={Object.assign(genericStyle, specificStyle)}>
         <OneDay/>
         <OneDay/>
         <OneDay/>
@@ -34,13 +44,12 @@ var ThreeDay = React.createClass({
 
 var TwoDay = React.createClass({
   render: function(){
-    var headerStyle = {
-      height: 20,
+    var specificStyle = {
       display:"inline-flex",
       flexDirection:'row'
     };
     return(
-      <div style={headerStyle}>
+      <div style={Object.assign(genericStyle, specificStyle)}>
         <OneDay/>
         <OneDay/>
       </div>
@@ -50,13 +59,12 @@ var TwoDay = React.createClass({
 
 var OneWeek = React.createClass({
   render: function(){
-    var headerStyle = {
-      height: 20,
+    var specificStyle = {
       display:"inline-flex",
       flexDirection:'row'
     };
     return(
-      <div style={headerStyle}>
+      <div style={Object.assign(genericStyle, specificStyle)}>
         <TwoDay/>
         <ThreeDay/>
         <TwoDay/>
@@ -67,13 +75,12 @@ var OneWeek = React.createClass({
 
 var TwoWeek = React.createClass({
   render: function(){
-    var headerStyle = {
-      height: 20,
+    var specificStyle = {
       display:"inline-flex",
-      flexDirection:'row'
+      flexDirection:'column'
     };
     return(
-      <div style={headerStyle}>
+      <div style={Object.assign(genericStyle, specificStyle)}>
         <OneWeek/>
         <OneWeek/>
       </div>
@@ -81,45 +88,57 @@ var TwoWeek = React.createClass({
   }
 });
 
-function MainView() {
-  if (zoomLevel < 20) {
-    return (
-      <div>
-        <TwoWeek/>
-        <TwoWeek/>
-      </div>
-    );
-  } else if (zoomLevel < 40) {
-    return (
-      <div>
-        <TwoWeek/>
-      </div>
-    );
-  } else if (zoomLevel < 60) {
-    return (
-      <div>
-        <OneWeek/>
-      </div>
-    );
-  } else if (zoomLevel < 80) {
-    return (
-      <div>
-        <ThreeDay/>
-      </div>
-    );
-  } else if (zoomLevel < 100) {
-    return (
-      <div>
-        <OneDay/>
-      </div>
-    );
-  } else if (zoomLevel = 100) {
-    return (
-      <div>
-        <Event/>
-      </div>
-    );
- }
-}
+var MainFullView = React.createClass({
+  render: function(){
+    var mainContainerStyle = {
+      padding: 10,
+      height: "80vh",
+      width: "vw",
+      border: "solid",
+      borderColor: "#321",
+      borderWidth: 1,
+      display:"inline-flex",
+      flexDirection:'column'
+    };
+    if (zoomLevel < 20) {
+      return (
+        <div style={mainContainerStyle}>
+          <TwoWeek/>
+          <TwoWeek/>
+        </div>
+      );
+    } else if (zoomLevel >= 20 && zoomLevel < 40) {
+      return (
+        <div style={mainContainerStyle}>
+          <TwoWeek/>
+        </div>
+      );
+    } else if (zoomLevel >= 40 && zoomLevel < 60) {
+      return (
+        <div style={mainContainerStyle}>
+          <OneWeek/>
+        </div>
+      );
+    } else if (zoomLevel >= 60 && zoomLevel < 80) {
+      return (
+        <div style={mainContainerStyle}>
+          <ThreeDay/>
+        </div>
+      );
+    } else if (zoomLevel >= 80 && zoomLevel < 100) {
+      return (
+        <div style={mainContainerStyle}>
+          <OneDay/>
+        </div>
+      );
+    } else if (zoomLevel = 100) {
+      return (
+        <div style={mainContainerStyle}>
+          This is it.
+        </div>
+      );
+   }
+  }
+});
 
-module.exports = MainView;
+module.exports = MainFullView;
