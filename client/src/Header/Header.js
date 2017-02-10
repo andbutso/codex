0f58be86ba4cc50e678d2d0ad8ebca071746cac6
George Mu <george.mu@gmail.com>
2017-02-10 15:27:18 -0500

    Converted header to ES6 standard

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from '../Images/Logo.jpg';
 import userPicture from '../Images/User.jpg';
 
-var LogoBox = React.createClass({
-  render: function(){
+class LogoBox extends React.component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
     var headerStyle = {
       height: 20,
       width: "10%",
@@ -16,38 +19,45 @@ var LogoBox = React.createClass({
       color: "#318CE7",
       flex: 1,
     };
-    return(
-      <div style={headerStyle}>
-        <div style={textStyle}>
+    return (
+      <div style={Object.assign(headerStyle)}>
+        <div style={Object.assign(textStyle)}>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <div style={textStyle}>
+        <div style={Object.assign(textStyle)}>
           Codex
         </div>
       </div>
     );
   }
-});
+}
 
-var SearchBox = React.createClass({
-  render: function(){
+class SearchBox extends React.component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
     var headerStyle = {
       height: 20,
-      width: "50%",
+      width: "40%",
       padding: 10,
       margin: 10,
       borderRadius:"3px",
     position:"center",
     };
-    return(
-      <input style={headerStyle} placeholder="search class...">
+
+    return (
+      <input style={Object.assign(headerStyle)} placeholder="search class...">
       </input>
     );
   }
-});
+}
 
-var UserBox = React.createClass({
-  render: function(){
+class UserBox extends React.component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
     var headerStyle = {
       height: 20,
       width: "10%",
@@ -60,42 +70,45 @@ var UserBox = React.createClass({
       color: "#318CE7",
       flex: 1,
     };
-    return(
-      <div style={headerStyle}>
-        <div style={textStyle}>
+    return (
+      <div style={Object.assign(headerStyle)}>
+        <div style={Object.assign(textStyle)}>
           <img src={userPicture} className="User-logo" alt="logo" />
         </div>
-        <div style={textStyle}>
+        <div style={Object.assign(textStyle)}>
           George
         </div>
       </div>
     );
   }
-});
+}
 
-var PageHeader = React.createClass({
-  render: function() {
-  var headerStyle = {
-    padding: 10,
-    margin: 10,
-    height: "10vh",
-    width: "vw",
-    backgroundColor: "#FFF",
-    borderBottomColor: "#318CE7",
-    borderBottomStyle:"solid",
-    borderWidth: 1,
-    display: "inline-flex",
-    justifyContent: 'space-between',
-    flexDirection: 'row'
-  };
-      return (
-        <div style={headerStyle}>
-          <LogoBox/>
-          <SearchBox/>
-          <UserBox/>
-        </div>
-      );
-    }
-});
+class PageHeader extends React.component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    var headerStyle = {
+      padding: 10,
+      margin: 10,
+      height: "10vh",
+      width: "vw",
+      backgroundColor: "#FFF",
+      borderBottomColor: "#318CE7",
+      borderBottomStyle:"solid",
+      borderWidth: 1,
+      display: "inline-flex",
+      justifyContent: 'space-between',
+      flexDirection: 'row'
+    };
+    return (
+      <div style={Object.assign(headerStyle)}>
+        <LogoBox/>
+        <SearchBox/>
+        <UserBox/>
+      </div>
+    );
+  }
+}
 
 module.exports = PageHeader;
